10424e89baa41f00956715683b59ecc2a0c4e284
Jeniffer Macena de Souza <61254473+jenmacena19@users.noreply.github.com>
2023-06-16 15:56:47 -0400

    fix: align item

--- src/app/components/boxLogin/boxLogin.tsx
+++ src/app/components/boxLogin/boxLogin.tsx
@@ -3,19 +3,34 @@ import Button from "../button/button";
 import Input from "../input/input";
 import Styles from "./styles.module.css"
 
-interface HandleBoxLogin{
-    title:string;
+interface HandleBoxLogin {
+    title: string;
 }
 
-export default function BoxLogin(props: HandleBoxLogin){
-    return(
+export default function BoxLogin(props: HandleBoxLogin) {
+    return (
         <div className={Styles.container_box}>
-            <Input type="text" text="nickname"/>
+            {/*<Input type="text" text="nickname"/>
             <Input type="text" text="email"/>
             <Input type="password" text="senha"/>
             <Button text='Registrar'/>
             <Link href='/'>
             </Link>
+            */}
+
+            <form>
+                <div>
+                    <label htmlFor="email">E-mail:</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="email" />
+                </div>
+
+            </form>
+            <Button text='Registrar' />
         </div>
     )
 
